b58ad464af9b4afadf0670f99a88062d3945eeb9
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-04-12 22:06:27 -0300

    add to load projects from server on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ProjectCard } from '../../components/ProjectCard/ProjectCard';
 
 import { Container } from './Home.styles';
 
+import { getProjects, deleteProject } from '../../services/projectService';
+import IProject from '../../models/project';
+
 export interface IHomeProps {}
 
 export function Home(props: IHomeProps) {
+  const [projects, setProjects] = useState<IProject[]>([]);
+
+  useEffect(() => {
+    loadProjects();
+  }, []);
+
+  const loadProjects = async () => {
+    const { data: loadProjects } = await getProjects();
+
+    setProjects(loadProjects);
+  };
+
   return (
     <Container>
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
+      {projects &&
+        projects.map(project => (
+          <ProjectCard title={project.name} projectId={project._id} />
+        ))}
     </Container>
   );
 }
